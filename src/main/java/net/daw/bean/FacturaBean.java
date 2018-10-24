9c5ae00ef8b9651eecbdd80b076fc9942e9506ee
jaume monzonis <jaume monzonis@LAPTOP-53UQI7G0>
2018-10-24 11:58:02 +0200

    todos los metodos para todas las clases

---
+++ src/main/java/net/daw/bean/FacturaBean.java
@@ -0,0 +1,38 @@
+package net.daw.bean;
+
+import java.util.Date;
+
+public class FacturaBean {
+	
+	private int id;
+	private Date fecha;
+	private float iva;
+	private int id_usuario;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public float getIva() {
+		return iva;
+	}
+	public void setIva(float iva) {
+		this.iva = iva;
+	}
+	public int getId_usuario() {
+		return id_usuario;
+	}
+	public void setId_usuario(int id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+
+}
---
+++ src/main/java/net/daw/bean/LineaBean.java
@@ -0,0 +1,35 @@
+package net.daw.bean;
+
+public class LineaBean {
+	
+	private int id;
+	private int cantidad;
+	private int id_producto;
+	private int id_factura;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	public int getId_producto() {
+		return id_producto;
+	}
+	public void setId_producto(int id_producto) {
+		this.id_producto = id_producto;
+	}
+	public int getId_factura() {
+		return id_factura;
+	}
+	public void setId_factura(int id_factura) {
+		this.id_factura = id_factura;
+	}
+
+}
