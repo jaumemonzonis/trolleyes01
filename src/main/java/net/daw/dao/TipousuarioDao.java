565c9b834394a224b7a0b285b5b440cfed62be3b
jaume monzonis <jaume monzonis@LAPTOP-53UQI7G0>
2018-10-24 12:13:16 +0200

    Todos los metodos y clases ok. v2

--- src/main/java/net/daw/dao/TipousuarioDao.java
+++ src/main/java/net/daw/dao/TipousuarioDao.java
@@ -7,42 +7,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import net.daw.bean.TipousuarioBean;
-    Connection oConnection;
-    String ob = "tipoUsuario";
 
-    public TipousuarioDao(Connection oConnection) {
-        super();
-        this.oConnection = oConnection;
-    }
-
-    public TipousuarioBean get(int id) throws Exception {
-        String strSQL = "SELECT * FROM " + ob + " WHERE id=?";
-        TipousuarioBean oTipousuarioBean;
-        ResultSet oResultSet = null;
-        PreparedStatement oPreparedStatement = null;
-        try {
-            oPreparedStatement = oConnection.prepareStatement(strSQL);
-            oPreparedStatement.setInt(1, id);
-            oResultSet = oPreparedStatement.executeQuery();
-            if (oResultSet.next()) {
-                oTipousuarioBean = new TipousuarioBean();
-                oTipousuarioBean.setId(oResultSet.getInt("id"));
-                oTipousuarioBean.setDesc(oResultSet.getString("desc"));
-            } else {
-                oTipousuarioBean = null;
-            }
-        } catch (SQLException e) {
-            throw new Exception("Error en Dao get de tipousuario", e);
-        } finally {
-            if (oResultSet != null) {
-                oResultSet.close();
-            }
-            if (oPreparedStatement != null) {
-                oPreparedStatement.close();
-            }
-        }
-
-        return oTipousuarioBean;
+public class TipousuarioDao {
 
 	Connection oConnection;
 	String ob = null;
@@ -148,57 +114,7 @@ import net.daw.bean.TipousuarioBean;
 				oPreparedStatement.close();
 			}
 		}
-    public int getCount() throws Exception {
-        String strSQL = "SELECT COUNT(id) FROM " + ob;
-        PreparedStatement oPreparedStatement = null;
-        ResultSet oResultSet = null;
-        int result = 0;
-        try {
-            oPreparedStatement = oConnection.prepareStatement(strSQL);
-            oResultSet = oPreparedStatement.executeQuery(strSQL);
-            if (oResultSet.next()) {
-                result = oResultSet.getInt(1);
-            }
-        } catch (SQLException e) {
-            throw new Exception("Error en Dao count de tipousuario " + e);
-        } finally {
-            if (oPreparedStatement != null) {
-                oPreparedStatement.close();
-            }
-            if (oResultSet != null) {
-                oResultSet.close();
-            }
-        }
-        return result;
-}
-    public boolean create (String desc) throws Exception {
-    	
-    	String strSQL = "INSERT INTO " + ob + " set " + ob + ".desc=?";
-        PreparedStatement oPreparedStatement = null;      
-        boolean result = false;
-        try {
-            oPreparedStatement = oConnection.prepareStatement(strSQL);
-            oPreparedStatement.setString(1, desc);
-            int validar=oPreparedStatement.executeUpdate();
-            if (validar==1) {
-            	result=true;
-            	}  
-            
-        } catch (SQLException e) {
-            throw new Exception("Error en Dao new de tipousuario", e);
-        } finally {
-//			if (oResultSet!=null) {
-//				oResultSet.close();
-//			}
-            if (oPreparedStatement != null) {
-                oPreparedStatement.close();
-            }
-        }
-        return result;
-    } 
-    
-    public boolean delete(int id) throws Exception {
-        String strSQL = "DELETE FROM " + ob + " WHERE id=?";
+		return oTipousuarioBean;
 	}
 
 	public int update(TipousuarioBean oTipousuarioBean) throws Exception {
@@ -254,56 +170,6 @@ import net.daw.bean.TipousuarioBean;
 		}
 		return alTipousuarioBean;
 
-        //TipousuarioBean oTipousuarioBean;
-        //ResultSet oResultSet = null;
-        PreparedStatement oPreparedStatement = null;      
-        boolean result = false;
-        try {
-            oPreparedStatement = oConnection.prepareStatement(strSQL);
-            oPreparedStatement.setInt(1, id);
-            int validar= oPreparedStatement.executeUpdate();
-            if (validar!=0) {
-            	result=true;
-            	}  
-        } catch (SQLException e) {
-            throw new Exception("Error en Dao delete de tipousuario", e);
-        } finally {
-//			if (oResultSet!=null) {
-//				oResultSet.close();
-//			}
-            if (oPreparedStatement != null) {
-                oPreparedStatement.close();
-            }
-        }
-        return result;
-    }
-    
-    public boolean update (int id,String desc) throws Exception {
-        String strSQL = "UPDATE " + ob + " set " + ob + ".desc=? where id =?";
+	}
 
-        PreparedStatement oPreparedStatement = null;      
-        boolean result = false;
-        try {
-            oPreparedStatement = oConnection.prepareStatement(strSQL);
-               oPreparedStatement.setInt(2, id);
-               oPreparedStatement.setString(1, desc); 
-             
-         
-            int validar= oPreparedStatement.executeUpdate();
-           
-            if (validar!=0) {
-            	result=true;
-            	}  
-        } catch (SQLException e) {
-            throw new Exception("Error en Dao update de tipousuario", e);
-        } finally {
-//			if (oResultSet!=null) {
-//				oResultSet.close();
-//			}
-            if (oPreparedStatement != null) {
-                oPreparedStatement.close();
-            }
-        }
-        return result;
-    }
 }
