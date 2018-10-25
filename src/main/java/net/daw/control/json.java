cf27e50e0c522d6236448150950e57b22087282d
jaume monzonis <jaume monzonis@LAPTOP-53UQI7G0>
2018-10-25 13:40:30 +0200

    Prueba enviar json al servidor, Contraseña encriptada

--- src/main/java/net/daw/control/json.java
+++ src/main/java/net/daw/control/json.java
@@ -42,7 +42,7 @@ public class json extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 
